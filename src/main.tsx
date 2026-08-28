adc5d65b7c11d78f03210269dffc50becd778c13
dspear425 <167424845+dspear425@users.noreply.github.com>
2026-08-28 18:54:28 -0500

    Use AppV6 dashboard notifications

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './notifications.css'
-import App from './AppV5'
+import App from './AppV6'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
