ccd02fd0c9ffd343ae9f16cc461ec189727eecf7
dspear425 <167424845+dspear425@users.noreply.github.com>
2026-08-28 19:02:35 -0500

    Load safety styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './notifications.css'
+import './safety.css'
 import App from './AppV6'
 
 createRoot(document.getElementById('root')!).render(
