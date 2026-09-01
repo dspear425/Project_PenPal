35f2edc86ec5f8ee0e706bd4e556847151a1ab21
dspear425 <167424845+dspear425@users.noreply.github.com>
2026-09-01 18:30:03 -0500

    Wire global mobile menu, connectivity, help context, and PWA registration

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './notifications.css'
 import './safety.css'
+import './helpContextBridge'
+import './pwa'
 import App from './AppRoot'
+import MobileActionMenu from './components/MobileActionMenu'
+import ConnectivityBanner from './components/ConnectivityBanner'
+import './mobile.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <MobileActionMenu />
+    <ConnectivityBanner />
   </StrictMode>,
 )
