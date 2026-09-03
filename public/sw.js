32567d01d7e05e418990b1b4518af5943e485aac
dspear425 <167424845+dspear425@users.noreply.github.com>
2026-09-02 20:48:24 -0500

    Bump PWA shell cache after mobile menu fix

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'project-penpal-shell-'
-const CACHE_VERSION = 'v2'
+const CACHE_VERSION = 'v3'
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_VERSION}`
 const APP_SHELL = [
   '/',
