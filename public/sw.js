fc396061d530ea8dfa8e8af914def13810cfac2a
dspear425 <167424845+dspear425@users.noreply.github.com>
2026-09-02 21:19:21 -0500

    Bump PWA cache for legal policy release

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'project-penpal-shell-'
-const CACHE_VERSION = 'v5'
+const CACHE_VERSION = 'v6'
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_VERSION}`
 const APP_SHELL = [
   '/',
