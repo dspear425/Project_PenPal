dd74ee78410e23fa9aea32cfdfa7610dd07009d1
dspear425 <167424845+dspear425@users.noreply.github.com>
2026-09-02 21:17:44 -0500

    Add signup legal consent state

---
+++ src/lib/legalSignupState.ts
@@ -0,0 +1,9 @@
+let signupLegalConsent = false
+
+export function setSignupLegalConsent(value: boolean) {
+  signupLegalConsent = value
+}
+
+export function hasSignupLegalConsent() {
+  return signupLegalConsent
+}
