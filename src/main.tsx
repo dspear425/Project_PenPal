d8a9cfa86d1534aeca7568b6d91786819d67b153
dspear425 <167424845+dspear425@users.noreply.github.com>
2026-09-02 21:15:20 -0500

    Add public legal footer and Help policy extension

--- src/main.tsx
+++ src/main.tsx
@@ -4,21 +4,25 @@ import './styles.css'
 import './notifications.css'
 import './safety.css'
 import './helpContextBridge'
+import './lib/legalHelpExtension'
 import './pwa'
 import App from './AppRoot'
 import MobileActionMenu from './components/MobileActionMenu'
 import ConnectivityBanner from './components/ConnectivityBanner'
 import LegalCenter from './components/LegalCenter'
 import LegalAcceptanceGate from './components/LegalAcceptanceGate'
+import LegalFooter from './components/LegalFooter'
 import './mobile.css'
 import './mobile-tablet.css'
 import './legal.css'
+import './legal-footer.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <LegalAcceptanceGate />
     <LegalCenter />
+    <LegalFooter />
     <MobileActionMenu />
     <ConnectivityBanner />
   </StrictMode>,
