3e1bc2e64e0f41dccb5d3abb1b9b5155372a325a
dspear425 <167424845+dspear425@users.noreply.github.com>
2026-09-10 18:27:47 -0500

    Bump PWA cache for Turnstile auth protection

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'project-penpal-shell-'
-const CACHE_VERSION = 'v15'
+const CACHE_VERSION = 'v16'
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_VERSION}`
 const APP_SHELL = [
   '/',
